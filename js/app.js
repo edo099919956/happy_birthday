4620c403dac622ac24dfb71132dc250af8037a63
Edgar Karapetyan <edo099919956@gmail.com>
2022-10-14 11:11:12 +0400

    add medio from phones

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-/* https://greensock.com/gsap */
-TweenLite.set("#petals", { perspective: 600 })
-TweenLite.set("img", { xPercent: "-50%", yPercent: "-50%" })
-
-var total = 50;
-var warp = document.getElementById("petals"),
-    w = window.innerWidth,
-    h = window.innerHeight;
-
-for (i = 0; i < total; i++) {
-    var Div = document.createElement('div');
-    TweenLite.set(Div, { attr: { class: 'dot' }, x: R(0, w), y: R(-200, -150), z: R(-200, 200) });
-    warp.appendChild(Div);
-    animm(Div);
-}
-
-function animm(elm) {
-    TweenMax.to(elm, R(6, 15), { y: h + 100, ease: Linear.easeNone, repeat: -1, delay: -15 });
-    TweenMax.to(elm, R(4, 8), { x: '+=100', rotationZ: R(0, 180), repeat: -1, yoyo: true, ease: Sine.easeInOut });
-    TweenMax.to(elm, R(2, 8), { rotationX: R(0, 360), rotationY: R(0, 360), repeat: -1, yoyo: true, ease: Sine.easeInOut, delay: -5 });
-};
-
-function R(min, max) { return min + Math.random() * (max - min) };
-
-let startDelayRund = { startDelayRund: 1000000 }
-
 function run() {
+    /* https://greensock.com/gsap */
+    TweenLite.set("#petals", { perspective: 600 })
+    TweenLite.set("img", { xPercent: "-50%", yPercent: "-50%" })
+
+    var total = 50;
+    var warp = document.getElementById("petals"),
+        w = window.innerWidth,
+        h = window.innerHeight;
+
+    for (i = 0; i < total; i++) {
+        var Div = document.createElement('div');
+        TweenLite.set(Div, { attr: { class: 'dot' }, x: R(0, w), y: R(-200, -150), z: R(-200, 200) });
+        warp.appendChild(Div);
+        animm(Div);
+    }
+
+    function animm(elm) {
+        TweenMax.to(elm, R(6, 15), { y: h + 100, ease: Linear.easeNone, repeat: -1, delay: -15 });
+        TweenMax.to(elm, R(4, 8), { x: '+=100', rotationZ: R(0, 180), repeat: -1, yoyo: true, ease: Sine.easeInOut });
+        TweenMax.to(elm, R(2, 8), { rotationX: R(0, 360), rotationY: R(0, 360), repeat: -1, yoyo: true, ease: Sine.easeInOut, delay: -5 });
+    };
+
+    function R(min, max) { return min + Math.random() * (max - min) };
+
+    let startDelayRund = { startDelayRund: 1000000 }
+
     document.querySelector('.card').setAttribute('style', 'animation: card 2s linear 3s forwards;')
     /* https://mattboldt.com/typed.js/ */
     var typed = new Typed('#text', {
@@ -48,20 +48,22 @@ function runStartDelay() {
     document.querySelector('.popup_img').addEventListener('click', () => {
 
         if (click_count == 3) {
+            document.querySelector('.popup_img').classList.add('popup_img_anim')
             document.querySelector('.popup').setAttribute('style', 'z-index: -10; opacity: 0;')
             setInterval(() => {
                 document.body.setAttribute('style', 'perspective: 1000px; font-family: "Caveat", cursive;')
                 document.querySelector('.popup').setAttribute('style', 'display: none;')
-            }, 800);
+            }, 600);
             run()
 
         } else {
-            if (click_count==0) {
-                document.querySelector('.popup_img').setAttribute('style', 'width:150px')
-            } else if(click_count==1) {
-                document.querySelector('.popup_img').setAttribute('style', 'width:220px')
-            } else if(click_count==2) {
-                document.querySelector('.popup_img').setAttribute('style', 'width:300px')
+            if (click_count == 0) {
+                document.querySelector('.popup_img').setAttribute('style', 'width:150px;height:150px')
+            } else if (click_count == 1) {
+                document.querySelector('.popup_img').setAttribute('style', 'width:220px;height:220px')
+            } else if (click_count == 2) {
+                document.querySelector('.popup_img').setAttribute('style', 'width:300px;height:300px')
+                document.querySelector('.popup_img').classList.add('popup_img_anim')
             }
         }
         click_count++
